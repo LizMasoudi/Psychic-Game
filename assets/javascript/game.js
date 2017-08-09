0073e66b6a268a81bf032dcce9497fba0e866cd7
Liz Masoudi <lizmasoudi@Lizs-MacBook-Pro-2.local>
2017-08-08 18:31:19 -0600

    refactoring v1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,12 @@ var winsJS = document.getElementById("wins");
 var lossesJS = document.getElementById("losses");
 var guessesRemainingJS = document.getElementById("guessesRemaining");
 var guessesJS = document.getElementById("guesses");
-
-
 var message = document.getElementById("message");
 
 var winsCounter = 0;
 var lossesCounter = 0;
 var guessesRemainingCounter = 9;
+var myGuess;
 
 winsJS.innerHTML = "Wins: " + winsCounter;
 lossesJS.innerHTML = "Losses: " + lossesCounter;
@@ -17,69 +16,61 @@ guessesRemainingJS.innerHTML = "Guesses Remaining: " + guessesRemainingCounter;
 
 // Randomly chooses a choice from the possibilities array, which becomes my guess.
 var possiblities = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var myGuess = possiblities[Math.floor(Math.random() * possiblities.length)+1];
+myGuess = possiblities[Math.floor(Math.random() * possiblities.length)+1];
 console.log(myGuess);
 
 
+// A reset function (reset guess counter and write to DOM, reset guess list and write to DOM
+// and pick a new letter for the computer guess)
+function reset() {
+	guessesRemainingCounter=9;
+	message.innerHTML="";
+	guessesJS.innerHTML="Guesses: ";
+
+}
+
+
+// An incorrect user guess (counter - 1, and write it to the DOM)
+function guess() {
+	guessesRemainingCounter--;
+	guessesRemainingJS.innerHTML = "Guesses Remaining: " + guessesRemainingCounter;
+}
+
+// A Win (counter + 1, write to the DOM, and write message to DOM indicating win)
+function win() {
+	winsCounter++;
+	winsJS.innerHTML = "Wins: " + winsCounter;
+	guessesRemainingJS.innerHTML = "Guesses Remaining: " + guessesRemainingCounter;
+	message.innerHTML="YOU WON!";
+	reset();
+
+}
+
+// A loss (counter - 1, write to the DOM, and write a message to DOM indicating loss)
+function lose () {
+	lossesCounter++;
+	lossesJS.innerHTML = "Losses: " + lossesCounter;
+	guessesRemainingJS.innerHTML = "Guesses Remaining: " + guessesRemainingCounter;
+	message.innerHTML="Uh oh, you lost...";
+	reset();
+
+}
+
 document.onkeyup = function(event) {
 
-  	// Determines which key was pressed.
+  	// Determines which key was pressed, and writes it to the DOM
   	var userGuess = event.key;
-  	console.log(userGuess);
   	var guessesTracker = document.createElement("p");
 	guessesTracker.innerHTML = (userGuess + ", ");
 
-
-
-	function win() {
-		winsCounter++;
-		message.innerHTML="YOU WON!";
-
-	}
-
-	function reset() {
-		guessesRemainingCounter=9;
-		message.innerHTML="";
-		guessesJS.innerHTML="Guesses: ";
-	}
-
-	function lose () {
-		lossesCounter++;
-		message.innerHTML="Uh oh, you lost...";
-
-	}
-
-
 	if (guessesRemainingCounter > 1 && userGuess === myGuess) {
 		win();
-		winsJS.innerHTML = "Wins: " + winsCounter;
-		reset();
-		guessesRemainingJS.innerHTML = "Guesses Remaining: " + guessesRemainingCounter;
-		
-
-		console.log(winsCounter);
-		console.log(lossesCounter);
-		console.log(guessesRemainingCounter);
 	}
 	else if (guessesRemainingCounter > 1 && userGuess != myGuess) {
-		// console.log("NOT YET!");
-		guessesRemainingCounter--;
-		guessesRemainingJS.innerHTML = "Guesses Remaining: " + guessesRemainingCounter;
+		guess();
 		guessesJS.appendChild(guessesTracker);
-		console.log(guessesRemainingCounter);
 	}
-
 	else {
 		lose();
-		lossesJS.innerHTML = "Losses: " + lossesCounter;		
-		reset();
-		guessesRemainingJS.innerHTML = "Guesses Remaining: " + guessesRemainingCounter;
-
-		console.log(winsCounter);
-		console.log(lossesCounter);
-		console.log(guessesRemainingCounter);
-		console.log(guessesTracker);
 	}
-
-  
 }
